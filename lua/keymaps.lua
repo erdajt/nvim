17485cd814e58e645641313eff1631b88e343492
ErdajtSopjani <erdajtsopjani.tech@gmail.com>
2025-05-26 16:59:53 +0200

    add multicursor | change some binds

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -13,12 +13,6 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
--- TIP: Disable arrow keys in normal mode
-vim.keymap.set("n", "<left>", '<cmd>echo "nonono"<CR>')
-vim.keymap.set("n", "<right>", '<cmd>echo "nonono"<CR>')
-vim.keymap.set("n", "<up>", '<cmd>echo "nonono"<CR>')
-vim.keymap.set("n", "<down>", '<cmd>echo "nonono"<CR>')
-
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the lower window" })
